ed15c5d7e10fb37982861c1b1145cff07e716229
John Paulo Bonacua <j.bonacua@foodics.com>
2025-04-11 15:34:51 +0800

    Add Food class

--- lib/database_module.dart
+++ lib/database_module.dart
@@ -3,3 +3,12 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
+class Food {
+  String name;
+  int calories;
+
+  Food({required this.name, required this.calories});
+
+
+}
